c933171715e1988656f86346fb2e5ebe33660aa1
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2024-12-18 10:41:45 +0200

    Created window

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ import random
 def random_number():
     number = random.randint(1, 100)
     label.config(text=str(number))
+
+root = Tk()
+root.title("Random num")
+root.geometry("300x300")
+
+button = Button(root, text="Generate", command=random_number)
+button.pack(pady=20)
+
+label = Label(root, text="")
+label.pack()
+
+
+root.mainloop()
